1ded89256f75d4328cfa90d070500c34ebd00a25
Shashwat Singh <singhshashwat1002@gmail.com>
2022-04-17 16:13:14 +0530

    add relevant padding related code so that batching can be done appropriately

--- src/data.py
+++ src/data.py
@@ -1,8 +1,14 @@
 import torch
 from icecream import ic
 
-class Dataset():
-  def __init__(self, trainfile="/Volumes/Untitled/ud-treebanks-v2.9/UD_English-Atis/en_atis-ud-train.conllu"):
+PAD_DEPENDENCY = -1
+# the dependency set will use -1 tokens as a pad
+# The reason is that -1 refers to the dummy head
+
+BATCH_SIZE = 100
+
+class Dataset(torch.utils.data.Dataset):
+  def __init__(self, trainfile=None):
     self.trainfile = trainfile
     self.vocab = []
     self.freqs = {}
@@ -12,9 +18,11 @@ class Dataset():
     self.tags_to_indices = {tag: index for index, tag in enumerate(
                               ['ADJ',   'ADP', 'ADV',  'AUX',  'CCONJ', 'DET', 'INTJ',
                                'NOUN',  'NUM', 'PART', 'PRON', 'PROPN', 'PUNCT',
-                               'SCONJ', 'SYM', 'VERB', 'X'])}
+                               'SCONJ', 'SYM', 'VERB', 'X', 'NULL'])}
 
-  def get_data(self):
+    self.length_longest_sequence = 0
+    # to store the length of the longest sequence
+
     dataset = []
     words = []
     postags = []
@@ -22,43 +30,103 @@ class Dataset():
 
     freqs = {}
     with open(self.trainfile, "r") as f:
-      for line in f:
+      lines = f.readlines()
+
+      for line in lines:
+
+        len_last_read = len(words)
+        if (len_last_read > self.length_longest_sequence):
+          self.length_longest_sequence = len_last_read
+
+        # Comment line
         if (len(line) > 0 and line[0] == '#'):
           continue
 
-        line = line[:-1]
+        line = line.strip('\n')
+        # Non-empty line
         if (len(line) > 0):
           columns = line.split('\t')
 
-          words.append(columns[1])
-          heads.append(int(columns[6]))
-          postags.append(columns[3])
+          WORD_INDEX, POS_INDEX, HEAD_INDEX = 1, 3, 6
+          words.append(columns[WORD_INDEX])
+          postags.append(columns[POS_INDEX])
+          heads.append(int(columns[HEAD_INDEX]))
 
           try: freqs[columns[1]] += 1
           except KeyError: freqs[columns[1]] = 1
 
-
+        # Empty line implies that a sentence is finished
         else:
+          words.insert(0, '<BOS>')
+          postags.insert(0, 'NULL')
+          heads.insert(0, -1)
+          # This -1 does NOT mean that the head of <BOS> is the last word
+          # in the sentence. It means that the head of <BOS> is itself.
+
           dataset.append((words, postags, heads))
           self.vocab += words
           words = []
           postags = []
           heads = []
 
+    metatokens = [ '<BOS>', '<PAD>', '<UNK>' ]
+    for tok in metatokens:
+      freqs.update({tok: 1})
+
     self.vocab = list(set(self.vocab))
     self.vocab = list(filter(lambda x: freqs[x] > 0, self.vocab))
-    self.vocab.append('<UNK>')
+    self.vocab = sorted(self.vocab)
+
+    self.vocab += metatokens
+    # last three indices are for <BOS>, <PAD> and <UNK> respectively
+
     self.freqs = freqs
     self.words_to_indices = {word: index for index, word in enumerate(self.vocab)}
 
     for (words, tags, heads) in dataset:
       word_indices = [self.index(word) for word in words]
       tag_indices = [self.tags_to_indices[tag] for tag in tags]
-      heads_indices = [word_indices[i-1] if (i > 0) else -1 for i in heads]
+      # converts sentence indices to vocabulary indices
+      heads_indices = [word_indices[i] if i != -1 else -1 for i in heads]
 
       self.dataset.append((word_indices, tag_indices, heads_indices))
 
   def index(self, word):
     try: idx = self.words_to_indices[word]
-    except KeyError: idx = len(self.vocab) - 1
+    except KeyError: idx = self.words_to_indices['<UNK>']
     return idx
+
+  def __len__(self):
+    return len(self.dataset)
+
+  def __getitem__(self, index):
+    #ic((torch.tensor(self.dataset[index])).shape)
+    to_ret = self.dataset[index]
+    seq_length = len(to_ret[0])
+    amount_to_pad = self.length_longest_sequence - seq_length
+    to_ret_tensor = torch.tensor(to_ret)
+    to_ret_padded = torch.nn.functional.pad(to_ret_tensor, (0, amount_to_pad), "constant", 0)
+
+    to_ret_padded[0] = torch.nn.functional.pad(
+                                  to_ret_tensor[0],
+                                  (0, amount_to_pad),
+                                  "constant",
+                                  len(self.vocab)-2)
+
+    to_ret_padded[1] = torch.nn.functional.pad(to_ret_tensor[1],
+                                              (0, amount_to_pad),
+                                              "constant",
+                                              len(self.tags_to_indices)-1
+                                              )
+
+    to_ret_padded[2] = torch.nn.functional.pad(to_ret_tensor[2],
+                                              (0, amount_to_pad),
+                                              "constant",
+                                              PAD_DEPENDENCY,
+    )
+
+    return to_ret_padded
+
+# test_dataset = Dataset(trainfile='../data/UD_English-Atis/en_atis-ud-test.conllu')
+# test_dataloader = torch.utils.data.DataLoader(test_dataset, shuffle=True, batch_size=BATCH_SIZE)
+
