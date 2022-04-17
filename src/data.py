bf178adf304bfc18524b584c3fe1c56beeb26841
Shashwat Singh <singhshashwat1002@gmail.com>
2022-04-17 16:20:20 +0530

    Revert "Checking if I can push without VPN lmaoooooo"
    This reverts commit f4c2e9961d17a033768eb3d478adff58ea1aeca9.
    
    Turns out I can

--- src/data.py
+++ src/data.py
@@ -130,4 +130,3 @@ class Dataset(torch.utils.data.Dataset):
 # test_dataset = Dataset(trainfile='../data/UD_English-Atis/en_atis-ud-test.conllu')
 # test_dataloader = torch.utils.data.DataLoader(test_dataset, shuffle=True, batch_size=BATCH_SIZE)
 
-# CHECK VPN
