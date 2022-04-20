d33de99a1f1f2503d9e86e49d576a5b3d56e4e9a
Abhinav271828 <abhinav.menon@students.iiit.ac.in>
2022-04-20 17:35:15 +0530

    Add batching, embedding, BiLSTM to edgescorer

--- src/edgescorer.py
+++ src/edgescorer.py
@@ -4,33 +4,66 @@ import torch
 from icecream import ic
 
 class EdgeScorer(nn.Module):
-  def __init__(self, input_size, output_size):
+  def __init__(self, output_size, vocab_size, word_embedding_dimension, pos_embedding_dimension, hidden_size, num_pos_tags, sentence_length, vocab=None, words_to_indices=None, pos_tagger):
     nn.Module.__init__(self)
-    self.head_encoder = MLP(input_size, output_size)
-    self.dep_encoder = MLP(input_size, output_size)
+
+    self.vocab = vocab
+    self.words_to_indices = words_to_indices
+    self.pos_tagger = pos_tagger
+    # Object of POSTag class
+
+    self.word_embedding_layer = nn.Embedding(vocab_size, word_embedding_dimension)
+    self.pos_embedding_layer = nn.Embedding(num_pos_tags, pos_embedding_dimension)
+
+    self.lstm = nn.LSTM(word_embedding_dimension+pos_embedding_dimension, hidden_size,
+                        bidirectional=True,batch_first=True)
+
+    self.head_encoder = MLP(hidden_size*2, output_size)
+    self.dep_encoder = MLP(hidden_size*2, output_size)
+    # *2 because the LSTM is bidirectional
 
     self.weight_arc = nn.Linear(output_size, output_size, bias=False)
-    self.bias_arc = nn.Linear(output_size, 1, bias=False)
+    self.bias_arc = nn.Linear(output_size, sentence_length, bias=False)
+    # Treat these as just matrices, not NN layers
+    # It's just to make training and mat mult convenient afaiu
 
-  def forward(self, sentence_repr):
-    H_head = self.head_encoder(sentence_repr)
-    H_dep = self.dep_encoder(sentence_repr)
+  def forward(self, batch):
+    words_embedded = self.word_embedding_layer(batch)
 
-    Wa_Hd = self.weight_arc(H_dep).transpose(0,1)
-    Hh_Wa_Hd = torch.mm(H_head, Wa_Hd)
+    pos_tags_probabilities = self.pos_tagger(batch)
+    pos_tags = self.pos_tagger.predict(pos_tags_probabilities)
+    pos_embedded = self.pos_embedding_layer(pos_tags)
+
+    lstm_inputs = torch.cat((words_embedded, pos_embedded), dim=2)
+    # Concatenate each word's embedding to its POS tag embedding
+
+    lstm_outputs, (last_hidden_state, last_cell_state) = self.lstm(lstm_inputs)
+
+    H_head = self.head_encoder(lstm_outputs)
+    H_dep = self.dep_encoder(lstm_outputs)
+    # Head and dependent representations for all words
+    # H_head : [batch_size, sentence_length, output_size]
+    # H_dep : [batch_size, sentence_length, output_size]
+
+    # Following lines are all matrix multiplications
+    Wa_Hd = self.weight_arc(H_dep).transpose(1,2)
+    # Wa_Hd : [batch_size, output_size, sentence_length]
+    Hh_Wa_Hd = torch.matmul(H_head, Wa_Hd)
+    # Hh_Wa_Hd : [batch_size, sentence_length, sentence_length]
 
     Hh_B = self.bias_arc(H_head)
+    # Hh_B : [batch_size, sentence_length, sentence_length]
 
     scores = Hh_Wa_Hd + Hh_B
     # scores[i][j] = prob of w[i] being head of w[j]
 
+    # We want scores[i][j] = prob of w[j] being head of w[i]
+    scores = scores.transpose(1,2)
+
     return scores
 
-  def predict(self, sentence_repr):
-    edge_probs = self.forward(sentence_repr)
-    heads = edge_probs.transpose(0,1).argmax(dim=1)
+  def predict(self, batch):
+    scores = self.forward(batch)
 
-    return heads
-
-st = torch.tensor([[1.,2.,3.],[4.,5.,6.]]).float()
-es = EdgeScorer(3,5)
+    # Most likely head for each dependent
+    return scores.argmax(dim=2)
